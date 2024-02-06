e8dc387257d5e55843de8dd32f1405cc5a2f5e5f
raulcatalan95 <60223546+raulcatalan95@users.noreply.github.com>
2024-02-05 23:32:26 -0300

    feat: add firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDjcJbzRK7wZZgCipzT_Tcd2pMC1_rmOyQ",
-//   authDomain: "coderhouse-5da1a.firebaseapp.com",
-//   projectId: "coderhouse-5da1a",
-//   storageBucket: "coderhouse-5da1a.appspot.com",
-//   messagingSenderId: "1057249299968",
-//   appId: "1:1057249299968:web:c2dd52b5ceb6bf4d6ed2e5"
-// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_APIKEY,
   authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
