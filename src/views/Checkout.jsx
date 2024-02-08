7768c022801bdcf8e7c5bfdb9a5f8ad67123c09c
Raul Catalan <raulcatalan95@gmail.com>
2024-02-08 01:35:34 -0300

    feat: add validation in form

--- src/views/Checkout.jsx
+++ src/views/Checkout.jsx
@@ -6,6 +6,44 @@ import { CartContext } from "../context/CartContext";
 const Checkout = () => {
 const { productsCart } = useContext(CartContext);
 const [ totalValue, setTotalValue ] = useState(0);
+const [ isValidForm, setIsValidForm ] = useState(false);
+const [ isValidEmail, setIsValidEmail ] = useState(true);
+const [ isValidPhone, setIsValidPhone ] = useState(true);
+const [ formValues, setFormValues] = useState({
+    nombre: null,
+    email: null,
+    telefono: null,
+    direccion: null,
+})
+
+function handleInputChange(e) {
+    setFormValues({
+        ...formValues,
+        [e.target.name]: e.target.value
+    });
+};
+function validateEmail() {
+    const expresionRegularEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const validateEmailFormat = expresionRegularEmail.test(formValues.email);
+    if (formValues.email != null) {
+        setIsValidEmail(validateEmailFormat);
+    }
+};
+function validatePhone() {
+    const expresionRegularNumbers = /[0-9]{8,}/;
+    const validatePhoneFormat = expresionRegularNumbers.test(formValues.telefono);
+    if (formValues.telefono != null) {
+        setIsValidPhone(validatePhoneFormat);
+    }
+};
+function validateForm() {
+    const isAllComplete = Object.values(formValues).every(valor => valor != '' && valor != null);
+    validateEmail();
+    validatePhone();
+    // const isValid = isAllComplete && isValidEmail && isValidPhone;
+    setIsValidForm(isAllComplete);
+    console.log(isValidForm);
+};
 
 useEffect(() => {
     if (productsCart.length === 1) {
@@ -14,7 +52,9 @@ useEffect(() => {
         const sumTotal =  productsCart.reduce((acc, curr) => acc + curr.total, 0);
         setTotalValue(sumTotal.toFixed(2));
     }
-}, []);
+    validateForm();
+   
+}, [formValues]);
 
 if (productsCart.length <= 0) {
     return <Navigate to={'/'} />
@@ -26,23 +66,27 @@ if (productsCart.length <= 0) {
         <h4>Completa tus datos para terminar la compra</h4>
         <form>
             <label htmlFor="nombre" className="input-group">
-                Nombre completo
-                <input type="text" name="nombre" placeholder="Ingrese nombre" required />
+                Nombre
+                <input onChange={handleInputChange} type="text" name="nombre" placeholder="Ingrese nombre" required />
+                { formValues.nombre === '' && <span className="error-msg">Campo invalido</span> }
             </label>
             <label htmlFor="email" className="input-group">
                 Correo electronico
-                <input type="email" name="email" placeholder="Ingresa email" required />
+                <input onChange={handleInputChange} type="email" name="email" placeholder="Ingresa email" required />
+                { (formValues.email === '' || !isValidEmail) && <span className="error-msg">Correo invalido</span> }
             </label>
             <label htmlFor="telefono" className="input-group">
                 Teléfono
-                <input type="text" name="telefono" placeholder="Ingresa teléfono" required />
+                <input onChange={handleInputChange} type="text" name="telefono" placeholder="Ingresa teléfono" required />
+                { (formValues.telefono === '' || !isValidPhone) && <span className="error-msg">Minimo 8 numeros</span> }
             </label>
-            <label htmlFor="telefono" className="input-group">
+            <label htmlFor="direccion" className="input-group">
                 Dirección
-                <input type="text" name="telefono" placeholder="Ingresa Telefono" required />
+                <input onChange={handleInputChange} type="text" name="direccion" placeholder="Ingresa Telefono" required />
+                { formValues.direccion === '' && <span className="error-msg">Campo invalido</span> }
             </label>
             <div className="button-form">
-                <button disabled={true}>Confirmar Compra</button>
+                <button disabled={!isValidForm || !isValidEmail || !isValidPhone}>Confirmar Compra</button>
             </div>
         </form>
       </div>
